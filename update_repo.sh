945b42d937c8d7b57813b9fc82dd89ef8ca623f7
paul guichon <paul.guichon@iedparis8.net>
2016-03-11 11:52:51 +0100

    mise à jour

--- update_repo.sh
+++ update_repo.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
 apps=(
-    duck_docker
-    duck_crispy_forms
-    helpdesk
-    xadmin
-    django_apogee
-    duck_examen
-    foad
-    duck_inscription
-    duck_inscription_payzen
-    duck_personnel
-    duck_recruitment
-    duck_salle
-    duck_site
-    duck_svi
-    duck_theme_ied
-    gestion_info
-    duck_loader
+    "duck_docker"
+    "duck_crispy_forms"
+    "helpdesk"
+    "xadmin"
+    "django_apogee"
+    "duck_examen"
+    "foad"
+    "duck_inscription"
+    "duck_inscription_payzen"
+    "duck_personnel"
+    "duck_recruitment"
+    "duck_salle"
+    "duck_site"
+    "duck_svi"
+    "duck_theme_ied"
+    "gestion_info"
+    "duck_loader"
     )
 
-for app in $apps
+for app in ${apps[*]}
 do
 cd ../$app && git pull && cd ../duck_docker
-echo "ok"
 done
